ced1d19ae3036c68b49a9d6bef07b45e4fff9af0
Andrew Zhang <axz9582@github.com>
2025-03-24 02:24:16 -0700

    try using hashrouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import Experience from './pages/Experience';
 import Projects from './pages/Projects';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
 
--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -11,10 +11,10 @@ function NavBar() {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav" >
                         <Nav className="ms-auto justify-content-evenly" >
-                            <Nav.Link className="p-5" href="about">About</Nav.Link>
-                            <Nav.Link className="p-5" href="experience">Experience</Nav.Link>
-                            <Nav.Link className="p-5" href="projects">Projects</Nav.Link>
-                            <Nav.Link className="p-5" href="contact">Contact</Nav.Link>
+                            <Nav.Link className="p-5" href="#about">About</Nav.Link>
+                            <Nav.Link className="p-5" href="#experience">Experience</Nav.Link>
+                            <Nav.Link className="p-5" href="#projects">Projects</Nav.Link>
+                            <Nav.Link className="p-5" href="#contact">Contact</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
